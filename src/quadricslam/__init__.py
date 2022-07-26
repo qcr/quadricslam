6746d0b8ada26f97b10a6a61509e60b0bab55f37
Ben Talbot <b.talbot@qut.edu.au>
2022-07-27 09:35:36 +1000

    Structure library to add interface implementations

--- src/quadricslam/__init__.py
+++ src/quadricslam/__init__.py
@@ -1,8 +1,8 @@
 from .quadricslam import QuadricSlam, qi, xi
-from .interfaces.data_associator import DataAssociator
-from .interfaces.data_source import DataSource
-from .interfaces.detector import Detection, Detector
-from .interfaces.visual_odometry import VisualOdometry
+from .data_associator import DataAssociator
+from .data_source import DataSource
+from .detector import Detection, Detector
+from .visual_odometry import VisualOdometry
 from .visualisation import visualise
 
 from . import utils
